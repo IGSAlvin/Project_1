906fa5aa0562e2c28c3c37a16500248c384444db
IGSAlvin <angeloalvinagusta@students.ignatiusglobal.co.id>
2023-11-06 20:56:15 +0700

    mendapatkan API

--- project.py
+++ project.py
@@ -5,10 +5,24 @@ from PIL import Image, ImageTk
 import ttkbootstrap
 
 def get_weather(city):
-    API_key = ""
-    url = f""
+    API_key = "b103be549427acf08923cf74d502efda"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
     res = requests.get(url)
     
+    if res.status_code == 404:
+        messagebox.showerror("error", "City not found")
+        return None
+    
+    weather = res.json()
+    icon_id = weather['weather'][0]['icon']
+    temperature = weather['main']['temp'] - 273.15
+    description = weather['weather']['0']['description']
+    city = weather['name']
+    country = weather['sys']['country']
+
+    icon_url = f"https://openweathermap.org/img/wn/{icon_id}@2x.png"
+    return(icon_url, temperature, description, city, country)
+
 def city_search():
     city = city_fill.get()
     result = get_weather(city)
